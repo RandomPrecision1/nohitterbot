f3cb72a8e7c3f95b67bc1ae2c3d0891df3641fd1
RandomPrecision1 <vooodoochild1987@gmail.com>
2018-08-15 23:40:01 -0500

    try/except per game, change innings to 5

--- nono.py
+++ nono.py
@@ -18,24 +18,29 @@ while True:
 	
 	games = mlbgame.day(now.year,now.month,now.day)
 	for game in games:
-		game_id = game.game_id
-		overview = mlbgame.overview(game_id)
-		playerstats = mlbgame.player_stats(game_id)
-		
-		home = game.home_team
-		away = game.away_team
-		gameday = "http://mlb.mlb.com/mlb/gameday/index.jsp?gid=" + overview.gameday_link
-		
-		if overview.inning >= 7 and game.away_team_hits <= 0 and game_id not in home_nonos:
-			pitcher = playerstats.home_pitching[0].name_display_first_last
-			text = pitcher + ", of the " + home + " has pitched six hitless innings vs the " + away + ".\n\nGameday link: " + gameday
-			sub.submit("No-H****r Alert: " + pitcher, text)
-			home_nonos.append(game_id)
-		if overview.inning >= 7 and game.home_team_hits <= 0 and game_id not in away_nonos:
-			pitcher = playerstats.away_pitching[0].name_display_first_last
-			text = pitcher + ", of the " + away + " has pitched six hitless innings vs the " + home + ".\n\nGameday link: " + gameday
-			sub.submit("No-H****r Alert: " + pitcher, text)
-			away_nonos.append(game_id)
+		try:
+			game_id = game.game_id
+			overview = mlbgame.overview(game_id)
+			playerstats = mlbgame.player_stats(game_id)
+			
+			home = game.home_team
+			away = game.away_team
+			gameday = "http://mlb.mlb.com/mlb/gameday/index.jsp?gid=" + overview.gameday_link
+			
+			if overview.inning >= 6 and game.away_team_hits <= 0 and game_id not in home_nonos:
+				pitcher = playerstats.home_pitching[0].name_display_first_last
+				text = pitcher + ", of the " + home + " has pitched five hitless innings vs the " + away + ".\n\nGameday link: " + gameday
+				sub.submit("No-H****r Alert: " + pitcher, text)
+				home_nonos.append(game_id)
+			if overview.inning >= 6 and game.home_team_hits <= 0 and game_id not in away_nonos:
+				pitcher = playerstats.away_pitching[0].name_display_first_last
+				text = pitcher + ", of the " + away + " has pitched five hitless innings vs the " + home + ".\n\nGameday link: " + gameday
+				sub.submit("No-H****r Alert: " + pitcher, text)
+				away_nonos.append(game_id)
+		except KeyboardInterrupt:
+			break
+		except:
+			pass
 
 	print("Sleeping...")
 	time.sleep(5 * 60)
